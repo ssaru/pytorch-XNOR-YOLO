627012a13974307dd1fbb38b10c4e88d8eaa0fc2
Martin Hwang <dhhwang89@gmail.com>
2021-06-06 01:45:19 +0900

    fix: fix typo in learning script
     Refs:

--- train_tiny_xnor_yolo.sh
+++ train_tiny_xnor_yolo.sh
@@ -1,4 +1,4 @@
 knockknock slack \
 	--webhook-url https://hooks.slack.com/services/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 \
-	--channel dev_github_action_noti \	
+	--channel dev_github_action_noti \
 	python3 main.py train --dataset-config=conf/data/data.yml --model-config=conf/model/xnor-tiny-yolo.yml --runner-config=conf/training/xnor-tiny-yolo_training.yml
